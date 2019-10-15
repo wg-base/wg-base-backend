bf6f88a5c9c588389f4e73181948be89f6250ce2
wangliheng <springioc@yeah.net>
2019-10-15 18:26:35 +0800

    remove import

--- src/main/java/com/wg/base/backend/util/TokenUtils.java
+++ src/main/java/com/wg/base/backend/util/TokenUtils.java
@@ -2,7 +2,6 @@ package com.wg.base.backend.util;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 
