9e200984470b27845febe39806406efb8d048a05
wangliheng <springioc@yeah.net>
2019-10-15 15:22:18 +0800

    fix format

--- src/main/java/com/wg/base/backend/common/enums/Status.java
+++ src/main/java/com/wg/base/backend/common/enums/Status.java
@@ -7,18 +7,12 @@ public abstract class Status {
      * 过期时间相关枚举
      */
     public static enum ExpireEnum{
-        //未读消息的有效期为30天
-        UNREAD_MSG(30L, TimeUnit.DAYS)
-        ;
 
-        /**
-         * 过期时间
-         */
-        private Long time;
-        /**
-         * 时间单位
-         */
-        private TimeUnit timeUnit;
+        UNREAD_MSG(30L, TimeUnit.DAYS);//未读消息的有效期为30天
+
+        private Long time;//过期时间
+
+        private TimeUnit timeUnit;//时间单位
 
         ExpireEnum(Long time, TimeUnit timeUnit) {
             this.time = time;
