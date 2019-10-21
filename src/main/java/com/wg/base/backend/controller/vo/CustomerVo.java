7b4e8c5b091bc32bf377e958fedd44400a944c3a
wang-pro <springioc@yeah.net>
2019-10-21 17:40:55 +0800

    fix

--- src/main/java/com/wg/base/backend/controller/vo/CustomerVo.java
+++ src/main/java/com/wg/base/backend/controller/vo/CustomerVo.java
@@ -1,9 +1,10 @@
 package com.wg.base.backend.controller.vo;
 
+import com.wg.base.backend.domain.Customer;
 import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
 @Data
 @ApiModel
-public class CustomerVo {
+public class CustomerVo extends Customer {
 }
